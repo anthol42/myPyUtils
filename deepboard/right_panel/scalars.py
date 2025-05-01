0276bd0f2ca106c1713055ffdae5dd04a42fcd40
Anthony Lavertu <alavertu2@gmail.com>
2025-05-01 14:00:10 -0400

    Feat(make_lines): more general

--- deepboard/right_panel/scalars.py
+++ deepboard/right_panel/scalars.py
@@ -5,25 +5,7 @@ from datetime import datetime, timedelta
 from fasthtml.common import *
 from fh_plotly import plotly2fasthtml
 from deepboard.components import Legend, Smoother, ChartType
-
-def make_df(socket, tag) -> Tuple[pd.DataFrame, List[int], List[int]]:
-    scalars = socket.read_scalar("/".join(tag))
-    steps = [scalar.step for scalar in scalars]
-    value = [scalar.value for scalar in scalars]
-    repetition = [scalar.run_rep for scalar in scalars]
-    walltime = [scalar.wall_time for scalar in scalars]
-    epochs = [scalar.epoch for scalar in scalars]
-    df = pd.DataFrame({
-        "step": steps,
-        "value": value,
-        "repetition": repetition,
-        "walltime": walltime,
-        "epoch": epochs
-    })
-    available_rep = df["repetition"].unique()
-    available_epochs = df["epoch"].unique()
-    df = df.set_index(["step", "repetition"])
-    return df, available_rep, available_epochs
+from deepboard.utils import get_lines
 
 def make_step_lines(socket, splits: set[str], metric: str, keys: set[tuple[str, str]]):
     from __main__ import CONFIG
@@ -31,15 +13,15 @@ def make_step_lines(socket, splits: set[str], metric: str, keys: set[tuple[str, 
     for i, split in enumerate(splits):
         tag = (split, metric)
         if tag in keys:
-            df, available_rep, available_epochs = make_df(socket, tag)
-            for rep in available_rep:
-                rep_df = df.loc[(slice(None), rep), :]
+            reps = get_lines(socket, split, metric, key="step")
+
+            for rep_idx, rep in enumerate(reps):
                 lines.append((
-                    f'{split}_{rep}',
-                    rep_df.index.get_level_values("step"),
-                    rep_df["value"],
+                    f'{split}_{rep_idx}',
+                    rep["index"],
+                    rep["value"],
                     CONFIG.COLORS[i % len(CONFIG.COLORS)],
-                    rep_df["epoch"].values if len(available_epochs) > 1 else None,
+                    rep["epoch"],
                 ))
     return lines
 
@@ -49,15 +31,15 @@ def make_time_lines(socket, splits: set[str], metric: str, keys: set[tuple[str, 
     for i, split in enumerate(splits):
         tag = (split, metric)
         if tag in keys:
-            df, available_rep, available_epochs = make_df(socket, tag)
-            for rep in available_rep:
-                rep_df = df.loc[(slice(None), rep), :]
+            reps = get_lines(socket, split, metric, key="duration")
+
+            for rep_idx, rep in enumerate(reps):
                 lines.append((
-                    f'{split}_{rep}',
-                    rep_df["walltime"],
-                    rep_df["value"],
+                    f'{split}_{rep_idx}',
+                    rep["index"],
+                    rep["value"],
                     CONFIG.COLORS[i % len(CONFIG.COLORS)],
-                    rep_df["epoch"].values if len(available_epochs) > 1 else None,
+                    rep["epoch"],
                 ))
     return lines
 
--- deepboard/utils.py
+++ deepboard/utils.py
@@ -1,6 +1,7 @@
 from datetime import datetime, date
 import sqlite3
-
+from typing import *
+import pandas as pd
 
 def _adapt_date_iso(val):
     """Adapt datetime.date to ISO 8601 date."""
@@ -28,3 +29,34 @@ def prepare_db():
 
     sqlite3.register_converter("date", _convert_date)
     sqlite3.register_converter("datetime", _convert_datetime)
+
+def make_df(socket, tag: Tuple[str, str]) -> Tuple[pd.DataFrame, List[int], List[int]]:
+    scalars = socket.read_scalar("/".join(tag))
+    steps = [scalar.step for scalar in scalars]
+    value = [scalar.value for scalar in scalars]
+    repetition = [scalar.run_rep for scalar in scalars]
+    walltime = [scalar.wall_time for scalar in scalars]
+    epochs = [scalar.epoch for scalar in scalars]
+    df = pd.DataFrame({
+        "step": steps,
+        "value": value,
+        "repetition": repetition,
+        "duration": walltime,
+        "epoch": epochs
+    })
+    available_rep = df["repetition"].unique()
+    available_epochs = df["epoch"].unique()
+    df = df.set_index(["step", "repetition"])
+    return df, available_rep, available_epochs
+
+def get_lines(socket, split, metric, key: Literal["step", "duration"]):
+    out = []
+    df, available_rep, available_epochs = make_df(socket, (split, metric))
+    for rep in available_rep:
+        rep_df = df.loc[(slice(None), rep), :]
+        out.append({
+            "index": rep_df.index.get_level_values("step"),
+            "value": rep_df["value"],
+            "epoch": rep_df["epoch"].values if len(available_epochs) > 1 else None,
+        })
+    return out
